1915b4136ae12e963793c050941a81a38cd55587
csjkevin <csjkevin@outlook.com>
2021-09-17 15:56:36 +0800

    chore: update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,7 @@
-/* eslint-disable strict */
-
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-  },
-  extends: ['eslint-config-egg', 'prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
+  extends: ['eslint-config-egg', require.resolve('@umijs/fabric/dist/eslint')],
   rules: {
-    'space-before-function-paren': [
-      'error',
-      { anonymous: 'always', named: 'never', asyncArrow: 'always' },
-    ],
-    'comma-dangle': ['error', 'always-multiline'],
+    strict: 0,
+    'no-return-await': 0,
   },
 };
